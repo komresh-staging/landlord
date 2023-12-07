c93a01e874a4c469d5987cc859ed2b530b07bde2
connectwp <150893902+connectwp@users.noreply.github.com>
2023-12-07 18:14:03 +0500

    updaed fixed some issues

--- worker/api-v1/index.php
+++ worker/api-v1/index.php
@@ -1,23 +1,8 @@
-<?php //00507
-// 11.0 74
-if(!extension_loaded('ionCube Loader')){$__oc=strtolower(substr(php_uname(),0,3));$__ln='ioncube_loader_'.$__oc.'_'.substr(phpversion(),0,3).(($__oc=='win')?'.dll':'.so');if(function_exists('dl')){@dl($__ln);}if(function_exists('_il_exec')){return _il_exec();}$__ln='/ioncube/'.$__ln;$__oid=$__id=realpath(ini_get('extension_dir'));$__here=dirname(__FILE__);if(strlen($__id)>1&&$__id[1]==':'){$__id=str_replace('\\','/',substr($__id,2));$__here=str_replace('\\','/',substr($__here,2));}$__rd=str_repeat('/..',substr_count($__id,'/')).$__here.'/';$__i=strlen($__rd);while($__i--){if($__rd[$__i]=='/'){$__lp=substr($__rd,0,$__i).$__ln;if(file_exists($__oid.$__lp)){$__ln=$__lp;break;}}}if(function_exists('dl')){@dl($__ln);}}else{die('The file '.__FILE__." is corrupted.\n");}if(function_exists('_il_exec')){return _il_exec();}echo("Site error: the ".(php_sapi_name()=='cli'?'ionCube':'<a href="http://www.ioncube.com">ionCube</a>')." PHP Loader needs to be installed. This is a widely used PHP extension for running ionCube protected PHP code, website security and malware blocking.\n\nPlease visit ".(php_sapi_name()=='cli'?'get-loader.ioncube.com':'<a href="http://get-loader.ioncube.com">get-loader.ioncube.com</a>')." for install assistance.\n\n");exit(199);
+<?php
 
-?>
-HR+cP/UVDj9JP8lbAYgZyTluZ0Zl0F6n8Ap1QQYuWhNTT23xwJiHnm06p4E0uJt4oeaIvIhhFYl4
-E3cVubQUTAjAX2kTvtpsBhk07LpOV4HF4vZOWLMpIrA5GDIcVQdgYGYgVdQcOSF3twccASPw5Lf6
-U5xp39wUt7taIfeuM0EV63d+eZYAqCJVpEen0zuGIHfXW3xGkX05gJ53TFdaCJP/Mvbn8hydriRC
-MIqSc1S8P70rW34klonhmOW2V9BHWWCbOaqKDhbX5EZMH+LAm2P29v1MdQzbS4vWqTCekKYXa6vT
-NwuM7cra7KrYG+HbQIJ2SrpI94wU42Y6OjQcvbE2x9WH8fQ11AQzoVFXBZGOKW/+AwtRC2VWA9xe
-5OqJoawBAZwwWbLpuoBeYaeBPQCLy7cWNvJLUgeRzjU1gWLTywlcq3wO8KOLwidWIETr3TFfp+wR
-H0+q73D60flY33+pgBULsCs33so8cEy02FLr3tLx7GCCQizJbsSj8umiW5VAxMSfvodu2BFenLwK
-2s/nc8hpRdt/PkctO7jumtZoofSp02oRO2TaZiUnbhTsXAT3EUAwAIQ+2IbNwMS81TnYOKoAwkLa
-q+DEyeZ1CmfI98zqwRxAqS9AIvwZexSsUvrHSxpIo4A0TdClQX//Smxasq0vpuZaD9qUvimlD9Zq
-DrLQijGP/yrlggqebMqWMQxmPV+zV0bft9rv2haJhp65udP/45jr1r/zwjbpWkLHmsTWOwRk1I3X
-I3P2Goanm40siHFboKodTdzVbFj5h7TchgMZ6xvpcMY34STecT9eASBz7Q7JPBsce2sv81ezx4uF
-S8YBOSH3TqI+Vra5WOr74DWaFf5OQhDI4hmVQJuzRhgCDsRavvuC9O9GGRxMSpltuR5kE5NXDOXD
-mIgKZyEnEkpp1JiDgwCHM6IZcsZ3VjJ+zyFoU/7JXsFBACFqraiJmYEWkt5r4tGfTo1pphnXKjYn
-6WslDE9znDs6GdBwclrP2ldRH3NhEhWHpMTfov8oCpiFrohJWSi0rabJmBau116pawHibjEePcZ/
-LX+0rnFsi2SukcS74ujpYyXfrohzRN8TivqJwnMrH5ODs6xACMUJfi2VjSvdTmIlztPCIUdAVtB/
-gj7HP5QFkqe1YhE2otfiCmsEU5cX0LYrW3U4FjLNL9T4n03NhAzdGhHnvASCBqAMNYVyFjHQCnew
-YGPpj7Q629a0DrLuayHVKN1AlVQ6RSmjmeXoKGCF6DFaYrLr1AW/VDAS+1ZIIWq9TvXw5LfdX5kK
-4sGiJLhCOuHIkJriP8a=
+define( 'API_REQUEST', true );
+
+
+require_once( '../index.php' );
+require_once( 'configuration.php' );
+new p5;
